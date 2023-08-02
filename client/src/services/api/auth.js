873924c6ec2fca792305eaf97f2e424f6fb04226
GavPierce <gpierce@thesmythgroup.com>
2023-08-02 13:09:54 -0600

    test with string lits

--- client/src/services/api/auth.js
+++ client/src/services/api/auth.js
@@ -1,28 +1,41 @@
-import axios from 'axios'
-import BaseApiService from './base'
+import axios from "axios";
+import BaseApiService from "./base";
 
 class AuthService extends BaseApiService {
-  login (email, password) {
-    return axios.post(this.BASE_URL + 'auth/login', {
-      email: email,
-      password: password
-    },
-    {
+  login(email, password) {
+    return axios.post(
+      this.BASE_URL + "auth/login",
+      {
+        email: email,
+        password: password
+      },
+      {
+        withCredentials: true
+      }
+    );
+  }
+
+  logout() {
+    return axios.post(
+      this.BASE_URL + "auth/logout",
+      {},
+      { withCredentials: true }
+    );
+  }
+
+  verify() {
+    return axios.post(
+      `${this.BASE_URL}auth/verify`,
+      {},
+      { withCredentials: true }
+    );
+  }
+
+  clearOauthDiscord() {
+    return axios.delete(this.BASE_URL + "auth/discord", {
       withCredentials: true
-    })
-  }
-
-  logout () {
-    return axios.post(this.BASE_URL + 'auth/logout', {}, { withCredentials: true })
-  }
-
-  verify () {
-    return axios.post(this.BASE_URL + 'auth/verify', {}, { withCredentials: true })
-  }
-
-  clearOauthDiscord () {
-    return axios.delete(this.BASE_URL + 'auth/discord', { withCredentials: true })
+    });
   }
 }
 
-export default new AuthService()
+export default new AuthService();
